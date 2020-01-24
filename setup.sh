735e3ca16165b75eebf11c0ac2d0ee52b5ab30ce
sungwon.pino <sungwon.pino@gmail.com>
2020-01-24 15:21:35 +0900

    setup.sh : update setup script for current OE system
    * add many layer
    * remove poky distro
    * change init system systemd
    * etc
    
    Signed-off-by: pino-kim <sungwon.pino@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,53 +1,85 @@
 #!/bin/bash
+# Bootstrapper for buildbot slave
 
-# environment
 DIR="build"
 MACHINE="raspberrypi3-64"
 CONFFILE="conf/auto.conf"
-BITBAKE="rpi-test-image"
+BITBAKEIMAGE="core-image-minimal"
 
-# init poky
-echo "Init poky"
-. ./poky/oe-init-build-env $DIR
+# clean up the output dir
+#echo "Cleaning build dir"
+rm -rf $DIR
 
-# set CONFIGFILE 
-if [ -e $CONFFILE ]; then
-    rm -rf $CONFFILE
+# make sure sstate is there
+#echo "Creating sstate directory"
+#mkdir -p ~/sstate/$MACHINE
+
+# fix permissions set by buildbot
+#echo "Fixing permissions for buildbot"
+#umask -S u=rwx,g=rx,o=rx
+#chmod -R 755 .
+
+# Reconfigure dash on debian-like systems
+which aptitude > /dev/null 2>&1
+ret=$?
+if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
+	sudo aptitude install expect -y
+	expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
+elif [ "${0##*/}" = "dash" ]; then
+	echo "dash as default shell is not supported"
+	return
 fi
-cat <<EOF > $CONFFILE
-MACHINE = "${MACHINE}"
-DISTRO ?= "poky"
 
-PACKAGE_CLASSES ?= "package_rpm"
-EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
-USER_CLASSES ?= "buildstats image-mklibs image-prelink"
-PATCHRESOLVE = "noop"
-
-BB_DISKMON_DIRS ??= "\
-    STOPTASKS,${TMPDIR},1G,100K \
-    STOPTASKS,${DL_DIR},1G,100K \
-    STOPTASKS,${SSTATE_DIR},1G,100K \
-    STOPTASKS,/tmp,100M,100K \
-    ABORT,${TMPDIR},100M,1K \
-    ABORT,${DL_DIR},100M,1K \
-    ABORT,${SSTATE_DIR},100M,1K \
-    ABORT,/tmp,10M,1K"
-
-PACKAGECONFIG_append_pn-qemu-native = " sdl"
-PACKAGECONFIG_append_pn-nativesdk-qemu = " sdl"
-
-CONF_VERSION = "1"
-EOF
+# bootstrap OE
+echo "Init OE"
+export BASH_SOURCE="openembedded-core/oe-init-build-env"
+. ./openembedded-core/oe-init-build-env $DIR
 
 # add the missing layers
 echo "Adding layers"
 bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-python2
 bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-networking
-bitbake-layers add-layer ../meta-raspberrypi
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-openembedded/meta-networking/
+bitbake-layers add-layer ../meta-raspberrypi/
 
-echo "To build an image run"
-echo "-------------------------------"
-echo "bitbake rpi-test-image"
-echo "-------------------------------"
+
+# fix the configuration
+echo "Creating auto.conf"
+
+if [ -e $CONFFILE ]; then
+	    rm -rf $CONFFILE
+    fi
+    cat <<EOF > $CONFFILE
+MACHINE ?= "${MACHINE}" 
+
+# rootfs for debugging
+#IMAGE_GEN_DEBUGFS = "1"
+#IMAGE_FSTYPES_DEBUGFS = "tar.gz"
+
+EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
+EXTRA_IMAGE_FEATURES_append = " ssh-server-dropbear"
+EXTRA_IMAGE_FEATURES_append = " package-management"
+
+PACKAGECONFIG_append_pn-qemu-native = " sdl"
+PACKAGECONFIG_append_pn-nativesdk-qemu = " sdl"
+
+USER_CLASSES ?= "buildstats buildhistory buildstats-summary image-mklibs image-prelink"
+
+require conf/distro/include/no-static-libs.inc
+require conf/distro/include/yocto-uninative.inc
+require conf/distro/include/security_flags.inc
+
+INHERIT += "uninative"
+
+#  DISTRO_FEATURES append set 
+DISTRO_FEATURES_append = " largefile opengl ptest multiarch wayland pam  systemd "
+
+# Using systemd instead of SysVinit
+VIRTUAL-RUNTIME_init_manager = "systemd"
+DISTRO_FEATURES_BACKFILL_CONSIDERED += "sysvinit"
+
+HOSTTOOLS_NONFATAL_append = " ssh"
+EOF
+
