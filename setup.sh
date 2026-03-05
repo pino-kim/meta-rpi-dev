144725154daeb84bd964e479795fd15d58766644
pino-kim <sungwon.pino@gmail.com>
2026-03-05 23:56:48 +0900

    temp2

--- setup.sh
+++ setup.sh
@@ -1,97 +1,86 @@
 #!/bin/bash
-# Bootstrapper for buildbot slave
+# Yocto/OE setup script (source this file)
+# Usage:
+#   . ./meta-rpi-dev/setup.sh
+#   . ./meta-rpi-dev/setup.sh raspberrypi4-64 build-rpi4 core-image-weston
 
-DIR="build"
-MACHINE="raspberrypi3-64"
+# ---------- config (can be overridden by args) ----------
+DIR="${2:-build}"
+MACHINE="${1:-raspberrypi3-64}"
+BITBAKEIMAGE="${3:-core-image-minimal}"
 CONFFILE="conf/auto.conf"
-BITBAKEIMAGE="core-image-minimal"
 
-# clean up the output dir
-#echo "Cleaning build dir"
-#rm -rf $DIR
-
-# make sure sstate is there
-#echo "Creating sstate directory"
-#mkdir -p ~/sstate/$MACHINE
-
-# fix permissions set by buildbot
-#echo "Fixing permissions for buildbot"
-#umask -S u=rwx,g=rx,o=rx
-#chmod -R 755 .
-
-# Reconfigure dash on debian-like systems
-which aptitude > /dev/null 2>&1
-ret=$?
-if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
-	sudo aptitude install expect -y
-	expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
-elif [ "${0##*/}" = "dash" ]; then
-	echo "dash as default shell is not supported"
-	return
+# ---------- safety: must be sourced ----------
+# If executed, exit; if sourced, return works.
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
+    echo "ERROR: This script must be sourced:"
+    echo "  . ${BASH_SOURCE[0]}"
+    exit 1
 fi
 
-# bootstrap OE
-echo "Init OE"
-export BASH_SOURCE="openembedded-core/oe-init-build-env"
-. ./openembedded-core/oe-init-build-env $DIR
+# ---------- do NOT touch /bin/sh or interactive tty ----------
+# Old dash/expect block removed on purpose:
+# - It can mess up byobu/tmux PTY
+# - Build scripts should not reconfigure system shell
 
-# add the missing layers
+echo "Init OE (DIR=${DIR}, MACHINE=${MACHINE})"
+. ./openembedded-core/oe-init-build-env "${DIR}"
+
+# Now we are inside ${DIR}
 echo "Adding layers"
-bitbake-layers add-layer ../openembedded-core/meta
-bitbake-layers add-layer ../meta-openembedded/meta-oe
-bitbake-layers add-layer ../meta-openembedded/meta-python
-bitbake-layers add-layer ../meta-openembedded/meta-multimedia
-bitbake-layers add-layer ../meta-openembedded/meta-networking/
-bitbake-layers add-layer ../meta-raspberrypi/
-bitbake-layers add-layer ../meta-rpi-dev
+bitbake-layers add-layer ../openembedded-core/meta >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-openembedded/meta-oe >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-openembedded/meta-python >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-openembedded/meta-networking >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-raspberrypi >/dev/null 2>&1 || true
+bitbake-layers add-layer ../meta-rpi-dev >/dev/null 2>&1 || true
 
+echo "Creating auto.conf (${CONFFILE})"
+rm -f "${CONFFILE}"
 
-# fix the configuration
-echo "Creating auto.conf"
+cat <<EOF > "${CONFFILE}"
+# Auto-generated
+MACHINE ?= "${MACHINE}"
 
-if [ -e $CONFFILE ]; then
-	    rm -rf $CONFFILE
-    fi
-    cat <<EOF > $CONFFILE
-MACHINE ?= "${MACHINE}" 
+# Debugging convenience
+#EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
+EXTRA_IMAGE_FEATURES:append = " ssh-server-dropbear"
+EXTRA_IMAGE_FEATURES:append = " package-management"
 
-# rootfs for debugging
-#IMAGE_GEN_DEBUGFS = "1"
-#IMAGE_FSTYPES_DEBUGFS = "tar.gz"
+# QEMU UI (optional)
+PACKAGECONFIG:append:pn-qemu-native = " sdl"
+PACKAGECONFIG:append:pn-nativesdk-qemu = " sdl"
 
-EXTRA_IMAGE_FEATURES ?= "debug-tweaks"
-EXTRA_IMAGE_FEATURES_append = " ssh-server-dropbear"
-EXTRA_IMAGE_FEATURES_append = " package-management"
+# Keep INHERIT minimal.
+# NOTE: image-mklibs / image-prelink are not present in newer OE-Core -> do NOT add them.
+INHERIT:append = " buildstats buildhistory buildstats-summary uninative"
 
-PACKAGECONFIG_append_pn-qemu-native = " sdl"
-PACKAGECONFIG_append_pn-nativesdk-qemu = " sdl"
+# Avoid duplicate requires: defaultsetup.conf already includes these on newer series.
+# require conf/distro/include/no-static-libs.inc
+# require conf/distro/include/yocto-uninative.inc
+# require conf/distro/include/security_flags.inc
 
-USER_CLASSES ?= "buildstats buildhistory buildstats-summary image-mklibs image-prelink"
-
-require conf/distro/include/no-static-libs.inc
-require conf/distro/include/yocto-uninative.inc
-require conf/distro/include/security_flags.inc
-
-INHERIT += "uninative"
-
-#  DISTRO_FEATURES append set 
-DISTRO_FEATURES_append = " largefile opengl ptest multiarch wayland pam  systemd "
-
-# Using systemd instead of SysVinit
+# systemd
+DISTRO_FEATURES:append = " largefile opengl ptest multiarch wayland pam systemd"
 VIRTUAL-RUNTIME_init_manager = "systemd"
-DISTRO_FEATURES_BACKFILL_CONSIDERED += "sysvinit"
+DISTRO_FEATURES_BACKFILL_CONSIDERED:append = " sysvinit"
 
-HOSTTOOLS_NONFATAL_append = " ssh"
+HOSTTOOLS_NONFATAL:append = " ssh"
 
-# Add Rpi board configuration
-# Read this doc carefully : https://meta-raspberrypi.readthedocs.io/en/latest/extra-build-config.html
-IMAGE_FSTYPES_append = " rpi-sdimg"
-ENABLE_UART = "1" 
-RPI_USE_U_BOOT = "1" 
-ENABLE_I2C = "1" 
-ENABLE_SPI_BUS = "1" 
+# Raspberry Pi extras (meta-raspberrypi)
+IMAGE_FSTYPES:append:raspberrypi3-64 = " rpi-sdimg"
+IMAGE_FSTYPES:append:raspberrypi4-64 = " rpi-sdimg"
+IMAGE_FSTYPES:append:raspberrypi5 = " rpi-sdimg"
+ENABLE_UART = "1"
+RPI_USE_U_BOOT = "1"
+ENABLE_I2C = "1"
+ENABLE_SPI_BUS = "1"
 
-# FiX uart output cracked #3
 GPU_FREQ = "250"
 EOF
 
+echo "Done."
+echo "You are now in: $(pwd)"
+echo "Next:"
+echo "  bitbake ${BITBAKEIMAGE}"
